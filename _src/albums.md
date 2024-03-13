c9124af15969611c66552c94c6e0c5b643563f84
mkelly76 <mkellytpc@gmail.com>
2024-03-13 19:49:33 -0400

    Spotify access token

--- _src/albums.md
+++ _src/albums.md
@@ -1,6 +1,9 @@
 ---
 title: Albums
 layout: "page.njk"
+hasjs: 1
 ---
 
-# Albums 
+# For Your Listening Pleasure
+
+<iframe style="border-radius:12px" src="https://open.spotify.com/embed/artist/58lV9VcRSjABbAbfWS6skp?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
--- _src/js/script.js
+++ _src/js/script.js
@@ -1,6 +1,23 @@
 let countDownDate = new Date("Apr 26, 2024 00:00:00").getTime();
 let concerts = document.querySelector('#ticketmaster');
 let tmApiKey = '2RIlBzDzNA3fNQnRjm3ZdMIOynOvhF0l';
+const client_id = '7e8edb27c26b4cbb977ebdd229614081';
+const client_secret = '67a4d39645b24b1f9184a9c3f1579491';
+
+let access_token;
+async function getAccessToken() {
+  const response = await fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': 'Basic ' + btoa(client_id + ':' + client_secret)
+    },
+    body: 'grant_type=client_credentials'
+  });
+  const data = await response.json();
+  access_token = data.access_token;
+}
+getAccessToken();
 
 let x = setInterval(function() {
     let now = new Date().getTime();
